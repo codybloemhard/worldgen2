d6569973851f1fb5a89d0eb0aa374d3b674159b0
Cody Bloemhard <cblmhrd@live.nl>
2019-07-13 01:13:07 +0200

    shader erro log

--- src/shader.cpp
+++ src/shader.cpp
@@ -14,9 +14,26 @@ Shader::Shader(const char* vs, const char* fs){
     Shader::vs = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(Shader::vs, 1, &rvs, NULL);
     glCompileShader(Shader::vs);
+
+    int succ;
+    char log[512];
+    glGetShaderiv(Shader::vs, GL_COMPILE_STATUS, &succ);
+    if(!succ){
+        glGetShaderInfoLog(Shader::vs, 512, NULL, log);
+        printf("Error: Vertex shader could not compile:\n");
+        printf(log);
+    }
+
     Shader::fs = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(Shader::fs, 1, &rfs, NULL);
     glCompileShader(Shader::fs);
+
+    glGetShaderiv(Shader::fs, GL_COMPILE_STATUS, &succ);
+    if(!succ){
+        glGetShaderInfoLog(Shader::fs, 512, NULL, log);
+        printf("Error: Fragment shader could not compile:\n");
+        printf(log);
+    }
 
     Shader::sh = glCreateProgram();
     glAttachShader(Shader::sh, Shader::fs);
