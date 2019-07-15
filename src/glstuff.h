1e85a504d1779302756a3b645d7ec28e84d394af
Cody Bloemhard <cblmhrd@live.nl>
2019-07-15 16:38:07 +0200

    ebo

--- src/glstuff.h
+++ src/glstuff.h
@@ -1,4 +1,8 @@
 #ifndef glstuff_h
 #define glstuff_h
+#include "../deps/glad/glad.h"
+#include "/usr/include/GLFW/glfw3.h"
+
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,13 @@ void init();
 void update(double);
 void render();
 
-static float points[] = {
+static float vertices[] = {
     0.0f,  0.5f,  0.0f,
     0.5f, -0.5f,  0.0f,
     -0.5f, -0.5f,  0.0f
+};
+static int indices[] = {
+    0, 1, 2
 };
 static GLuint vao;
 static Shader* shader;
@@ -29,15 +32,20 @@ int main(){
 }
 
 void init(){
-    GLuint vbo = 0;
+    GLuint vbo;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     vao = 0;
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
     shader = new Shader("shaders/basic.vs", "shaders/basic.fs");
 }
@@ -47,8 +55,10 @@ void update(double elaps){
 }
 
 void render(){
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     shader->Use();
     glBindVertexArray(vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
 }
