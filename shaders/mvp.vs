3ba100b7363f89ff51d8678a5c85ad3c2342b9b5
Cody Bloemhard <cblmhrd@live.nl>
2019-07-15 22:46:52 +0200

    beginning camera

--- shaders/mvp.vs
+++ shaders/mvp.vs
@@ -2,8 +2,8 @@
 layout (location = 0) in vec3 vp;
 uniform mat4 model;
 uniform mat4 view;
-uniform mat4 projection;
+uniform mat4 proj;
 
 void main(){
-    gl_Position = projection * view * model * vec4(vp, 1.0);
+    gl_Position = proj * view * model * vec4(vp, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ static int indices[] = {
 };
 static GLuint vao, vbo, ebo;
 static Shader* shader;
+float pitch, yaw;
 
 int main(){
     printf("Henlo Frens!\n");
@@ -47,7 +48,12 @@ void init(){
 }
 
 void update(double elaps){
-
+    pitch = 0.0f;//+= elaps * 20.0f;
+    yaw += elaps * 50.0f;
+    if(pitch > 89.9f)
+        pitch = 89.9f;
+    if(pitch < -89.9f)
+        pitch = -89.9f;
 }
 
 void render(){
@@ -55,9 +61,22 @@ void render(){
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     shader->Use();
+    glm::vec3 campos = glm::vec3(0.0f, 0.0f, -5.0f);
+    glm::vec3 camdir = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    
+    camdir.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    camdir.y = sin(glm::radians(pitch));
+    camdir.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    camdir = glm::normalize(camdir);
+    glm::vec3 camri = glm::normalize(glm::cross(camdir, up));
+    glm::vec3 camup = glm::normalize(glm::cross(camri, camdir));
+    
+    glm::mat4 view = glm::lookAt(campos, campos + camdir, camup);
+    glm::mat4 proj = glm::perspective(glm::radians(90.0f), 16.0f/9.0f, .1f, 100.0f);
     shader->SetMat4("model", glm::mat4(1.0f));
-    shader->SetMat4("view", glm::mat4(1.0f));
-    shader->SetMat4("projection", glm::mat4(1.0f));
+    shader->SetMat4("view", view);
+    shader->SetMat4("proj", proj);
     glBindVertexArray(vao);
     glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
