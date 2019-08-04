a257301db213612e60ed9353461aee6096696a57
Cody Bloemhard <cblmhrd@live.nl>
2019-08-04 22:40:25 +0200

    coloring into vertex

--- shaders/terrain.fs
+++ shaders/terrain.fs
@@ -1,41 +1,10 @@
 #version 400
-uniform float height;
 uniform vec3 light_dir;
-in vec3 pos;
 in vec3 nor;
+in vec3 col;
 out vec4 frag_colour;
 
-float band(float value, float minh, float maxh, float edge){
-    return smoothstep(minh - edge, maxh + edge, value) * smoothstep(maxh + edge, maxh - edge, value);
-}
-
-vec3 mixlayer(vec3 botl, vec3 topl, float maxtop, float softness){
-    float power = clamp(dot(nor, vec3(0,-1,0)), 0, 1);
-    power = smoothstep(maxtop, maxtop + softness, power);
-    return mix(botl, topl, power);
-}
-
 void main(){
-    vec3 p = pos;
-    p.y /= height;
-    float h = p.y;
-    vec3 sand = vec3(0.79f,0.58f,0.21f);
-    vec3 grass = normalize(vec3(0.27f,0.48f,0.19f));
-    vec3 plain = normalize(vec3(0.36f,0.22f,0.19f));
-    vec3 snow = vec3(0.9f);
-    
-    float b = 0.09f;
-    float cband = 0, totalband = 0;
-    vec3 col;
-    cband = band(h, 0, 0.25, b); totalband += cband;
-    col += cband * sand;
-    cband = band(h, 0.25, 0.6, b); totalband += cband;
-    col += cband * mixlayer(plain, grass, 0.6, 0.4);
-    cband = band(h, 0.6, 0.8, b); totalband += cband;
-    col += cband * plain;
-    cband = band(h, 0.8, 1.0, b); totalband += cband;
-    col += cband * snow;
-    col /= totalband;
     float lpow = max(dot(nor, light_dir), 0.0);
     frag_colour = vec4(col * lpow, 1);
 }
--- shaders/terrain.vs
+++ shaders/terrain.vs
@@ -6,12 +6,39 @@ uniform mat4 view;
 uniform mat4 proj;
 uniform float height;
 
-out vec3 pos;
 out vec3 nor;
+out vec3 col;
+
+float band(float value, float minh, float maxh, float edge){
+    return smoothstep(minh - edge, maxh + edge, value) * smoothstep(maxh + edge, maxh - edge, value);
+}
+
+vec3 mixlayer(vec3 botl, vec3 topl, float maxtop, float softness){
+    float power = clamp(dot(nor, vec3(0,-1,0)), 0, 1);
+    power = smoothstep(maxtop, maxtop + softness, power);
+    return mix(botl, topl, power);
+}
 
 void main(){
     vec4 p = model * vec4(vp, 1.0);
     gl_Position = proj * view * p;
-    pos = p.xyz;
     nor = (model * vec4(nr, 1.0)).xyz;
+    float h = p.y / height;
+    vec3 sand = vec3(0.79f,0.58f,0.21f);
+    vec3 grass = normalize(vec3(0.27f,0.48f,0.19f));
+    vec3 plain = normalize(vec3(0.36f,0.22f,0.19f));
+    vec3 snow = vec3(0.9f);
+    
+    float b = 0.09f;
+    float cband = 0, totalband = 0;
+    col = vec3(0);
+    cband = band(h, 0, 0.25, b); totalband += cband;
+    col += cband * sand;
+    cband = band(h, 0.25, 0.6, b); totalband += cband;
+    col += cband * mixlayer(plain, grass, 0.6, 0.4);
+    cband = band(h, 0.6, 0.8, b); totalband += cband;
+    col += cband * plain;
+    cband = band(h, 0.8, 1.0, b); totalband += cband;
+    col += cband * snow;
+    col /= totalband;
 }
